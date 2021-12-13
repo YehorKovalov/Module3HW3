190f6d6b6607d368119b00d75bf61acb5cad8f71
MascotYehorKovalov <yehor.kovalov1@nure.ua>
2021-12-13 11:47:54 +0200

    stylecop

---
+++ DelegatePractice/DelegatePractice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DelegatePractice
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
