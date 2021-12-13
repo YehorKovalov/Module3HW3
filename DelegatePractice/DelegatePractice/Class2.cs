52e5b056d908802c353dd755488dbf62127b3422
MascotYehorKovalov <yehor.kovalov1@nure.ua>
2021-12-13 14:20:14 +0200

    Class1, Class2, Show in main

---
+++ DelegatePractice/DelegatePractice/Class2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DelegatePractice
+{
+    public class Class2
+    {
+        private double _calcPowResult;
+
+        public Predicate<double> Calc(Func<double, double, double> operation, double x, double y)
+        {
+            if (operation != null)
+            {
+                _calcPowResult = operation.Invoke(x, y);
+            }
+
+            return Result;
+        }
+
+        private bool Result(double x) => _calcPowResult % x == 0;
+    }
+}
--- DelegatePractice/DelegatePractice/Program.cs
+++ DelegatePractice/DelegatePractice/Program.cs
@@ -6,7 +6,19 @@ namespace DelegatePractice
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var c1 = new Class1();
+            var c2 = new Class2();
+            c1.ShowHandler = Show;
+            var calcRes = c2.Calc(Class1.Pow, 2, 4);
+            var check1 = calcRes.Invoke(3);
+            var check2 = calcRes.Invoke(4);
+            c1.ShowHandler.Invoke(check1);
+            c1.ShowHandler.Invoke(check2);
+        }
+
+        private static void Show(bool result)
+        {
+            Console.WriteLine($"Result: {result}");
         }
     }
 }
